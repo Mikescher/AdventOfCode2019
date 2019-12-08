dc3275a8f904afdcca0e909e6a294b81b4214fc0
Mike Schwörer <mailport@mikescher.de>
2019-12-08 15:55:20 +0100

    Day 08 - Problem 2

--- 08_solution-2.ts
+++ 08_solution-2.ts
@@ -8,6 +8,36 @@ namespace AdventOfCode2019_08_2
 		let input = await AdventOfCode.getInput(DAY);
 		if (input == null) return;
 
-        AdventOfCode.output(DAY, PROBLEM, "TODO");
+		let data = input.trim().split("").map(p => parseInt(p));
+
+		const layers = [];
+
+		while (data.length>0) { layers.push(data.slice(0, 25*6)); data = data.slice(25*6); }
+
+		let r = Array(25*6); 
+		for(let i=0; i<25*6;i++) r[i]=2;
+		
+		for(let il = layers.length-1; il >= 0 ; il--)
+		{
+			for(let i=0; i<25*6;i++)
+			{
+				if (layers[il][i] == 2) continue;
+				r[i] = layers[il][i];
+			}
+		}
+
+		let str = "";
+		for(let i=0; i<25*6;i++)
+		{
+			if (i>0 && i%25==0)str+="\n";
+
+			if (r[i]===0) str += " ";
+			if (r[i]===1) str += "#";
+			if (r[i]===2) str += ".";
+		}
+
+		console.log(str);
+
+		AdventOfCode.output(DAY, PROBLEM, "CJZLP"); // OCR -.-
 	}
 }
